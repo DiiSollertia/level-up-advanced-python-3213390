9b5fae4d0504c91f95e06c1e56e90e979ceed719
Khoo Xin Zhen <70631933+DiiSollertia@users.noreply.github.com>
2023-04-05 09:11:48 +0000

    Completed html2markdown challenge

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,16 @@
+import re
+
 
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+    # This isn't just a dictionary, it's a lexicon!
+    html2md = {
+        r"<em>(.+?)</em>": r"*\1*",
+        r"\s+": r" ",
+        r"(?<=<p>)(.*?</p>?)(?=.*?<p>)": r"\1\n\n",
+        r"</?p>": r"",
+        r'<a href="(\S+?)">(.+?)</a>': r"[\2](\1)"
+    }
+    for key, values in html2md.items():
+        html = re.compile(key).sub(values, html)
+    return html
