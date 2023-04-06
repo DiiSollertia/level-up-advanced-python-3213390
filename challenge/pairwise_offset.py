77f8766e57b1ce95d4f049e3e6e67d429cbb2db7
Khoo Xin Zhen <70631933+DiiSollertia@users.noreply.github.com>
2023-04-06 00:21:58 +0000

    Completed pairwise_offset challenge

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,7 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence: list, fillvalue: str = '*', offset: int = 0) -> tuple:
+    # Func accepts all iterables
+    if not isinstance(sequence, list):
+        # Converted to list to prevent TypeError
+        sequence = list(sequence)
+    fill = [fillvalue] * offset
+    return list(zip(sequence+fill, fill+sequence))
