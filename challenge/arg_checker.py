c15d6f090998376b9863cddfe2fca0c7d5d0be92
Khoo Xin Zhen <70631933+DiiSollertia@users.noreply.github.com>
2023-04-06 05:02:06 +0000

    Completed arg_checker challenge

--- challenge/arg_checker.py
+++ challenge/arg_checker.py
@@ -1,7 +1,16 @@
+import functools
+
 
 def arg_checker(*arg_types):
     '''An argument checker decorator that checks both:
         - The number of variables that you use for a function
         - The type of each variable.
-       Raises a TypeError if either of these fail''' 
-    pass       
+       Raises a TypeError if either of these fail'''
+    def decorator(func):
+        @functools.wraps(func)
+        def check(*args):
+            if len(args) != len(arg_checker) or any(not isinstance(x, arg_types[i]) for i, x in enumerate(args)):
+                raise TypeError
+            return func(*args)
+        return check
+    return decorator
